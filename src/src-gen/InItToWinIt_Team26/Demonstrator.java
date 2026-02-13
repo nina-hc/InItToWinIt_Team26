92db0a60af8e8c44276a39ffb77905880273656d
Marvahassan <marvahassan94@gmail.com>
2026-02-13 15:55:48 -0500

    demonstrator addijg

---
+++ src/src-gen/InItToWinIt_Team26/Demonstrator.java
@@ -0,0 +1,25 @@
+package InItToWinIt_Team26;
+
+public class Demonstrator {
+
+        public static void main(String[] args) {
+            Game game = new Game(10);
+
+            // initial placement phase
+            game.initialPlacement();
+
+            //start simulation
+            game.startSimulation();
+
+
+
+
+
+            }
+
+
+
+
+
+
+}
