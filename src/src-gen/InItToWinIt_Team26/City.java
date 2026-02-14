fcdc14a4d03ccdcfaea3691d1eba64b4c14782c3
Nina HC <haycoopn@mcmaster.ca>
2026-02-13 21:39:50 -0500

    updating files with some comments

--- src/src-gen/InItToWinIt_Team26/City.java
+++ src/src-gen/InItToWinIt_Team26/City.java
@@ -1,9 +1,17 @@
 package InItToWinIt_Team26;
 
+/**Class to represent cities in Catan
+ * @author Nina Hay Cooper, February 13th 2026
+ */
 public class City {
     private Node node;//where its located
     private int ownerID;//playerID that owns it
 
+    /**Constructor
+     * 
+     * @param node nodeID for the city
+     * @param ownerID playerID who owns the city
+     */
     public City(Node node, int ownerID){
         this.node=node;
         this.ownerID=ownerID;
