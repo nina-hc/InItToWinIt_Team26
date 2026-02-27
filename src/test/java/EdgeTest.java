c764c385fce6a5c9daa90c472ac0a41ff15d8c14
Nina HC <haycoopn@mcmaster.ca>
2026-02-27 17:27:59 -0500

    mannually adding tests to try and fix

---
+++ src/test/java/EdgeTest.java
@@ -0,0 +1,44 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class EdgeTest {
+	@Test
+	void name() {
+	}
+
+	@Test
+	void getNodeA() {
+	}
+
+	@Test
+	void getNodeB() {
+	}
+
+	@Test
+	void getEdgeID() {
+	}
+
+	@Test
+	void getRoad() {
+	}
+
+	@Test
+	void hasRoad() {
+	}
+
+	@Test
+	void placeRoad() {
+	}
+
+	@Test
+	void getOppositeNode() {
+	}
+
+	@Test
+	void sharesNode() {
+	}
+
+	@Test
+	void isAdjacentTo() {
+	}
+}
