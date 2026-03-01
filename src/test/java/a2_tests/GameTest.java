e150471f9fe174509fd2030e9753d640cf07f9a6
Nina HC <haycoopn@mcmaster.ca>
2026-02-28 22:32:00 -0500

    adding partial tests to check sonar

---
+++ src/test/java/a2_tests/GameTest.java
@@ -0,0 +1,21 @@
+package a2_tests;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class GameTest {
+
+	@BeforeEach
+	void setUp() {
+	}
+
+	@Test
+	void initialPlacement() {
+	}
+
+	@Test
+	void startSimulation() {
+	}
+}
