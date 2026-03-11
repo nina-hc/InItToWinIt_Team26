8dcf21ad43febea572dfec0ab1a591d66269a2bf
Nina HC <haycoopn@mcmaster.ca>
2026-03-10 22:55:15 -0400

    commiting before changing file names

---
+++ src/test/java/a2_tests/PlacementValidatorTest.java
@@ -0,0 +1,33 @@
+package a2_tests;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PlacementValidatorTest {
+
+	@BeforeEach
+	void setUp() {
+	}
+
+	@Test
+	void canPlaceSettlement() {
+	}
+
+	@Test
+	void getValidSettlementPlacements() {
+	}
+
+	@Test
+	void canUpgradeToCity() {
+	}
+
+	@Test
+	void canBuildRoad() {
+	}
+
+	@Test
+	void getValidRoadEdges() {
+	}
+}
